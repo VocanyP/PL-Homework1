3d547db3be9b3e50b01294b71a1415062d655d79
Vocany Pitia <mr.vocanypitia@gmail.com>
2018-09-12 12:41:57 -0500

    nothing

--- Motzkin Tree.py
+++ Motzkin Tree.py
@@ -2,8 +2,6 @@
 #CSCE 4430
 #Assignment 1
 
-final = []
-
 #Motzkin Tree of Size n
 def mot(n):
     if n == 0:
@@ -17,37 +15,31 @@ def mot(n):
                     yield ('binary',l,r)
 
 #Motzkin Tree of Size n
-def mot2(j):
-    result = []
+def mymot(j):
+    result = []             #Holds the result for each tree of size N
     if j == 0:
         result.append('leaf')
-        final.append(result)
-        #return result
     else:
-        for v in mot2(j-1):
+        for v in mymot(j-1):
             result.append(tuple(('unary',v)))
-            #print("Value of v", v)
-            #print("Result in v", result)
         for k in range(0, j-1):
-            for l in mot2(k):
-                for r in mot2(j-2-k):
+            for l in mymot(k):
+                for r in mymot(j-2-k):
                     result.append(tuple(('binary',l,r)))
-                    final.append(result)
-                    #return result
-    final.append(result)
     return result
+
 def test(m):
     for n in range(m):
         list(map(print,list(mot(n))))
 
-def test2(a):
-    for x in range(a+1):
-        print(mot2(x))
-    #print(final)
-
+#My test function to test mymot
+def mytest(a):
+    for x in range(a):
+        for y in mymot(x):
+            print(y)
 
 if __name__ == '__main__':
     test(4)
     print("\n\n\n")
-    test2(3)
+    mytest(4)
 
